379955b97862fe10f071e03259b413ca7d89e33e
JuanCodeJS <jchachinoy@uninorte.edu.co>
2022-11-19 18:56:13 -0500

    generacion de proyecto con react y bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { BrowseRouter, Routes, Route} from "react-router-dom"
+
+
+function App() {
+
+  return (
+    <button type="button" class="btn btn-primary">Primary</button>
+  )
+}
+
+export default App
